028d082dd2b8a7898b0dc5f8a840791b1b93438a
shafu.eth <selfouly@gmail.com>
2022-11-21 00:05:37 +0100

    typo

--- src/Pool.sol
+++ src/Pool.sol
@@ -192,7 +192,7 @@ contract Pool {
 
   /// @notice Mint dyad to the NFT
   function mintDyad(uint minAmount) payable external onlyNFT returns (uint) {
-    require(msg.value > 0,        "Pool: You need to send some ETH");
+    require(msg.value > 0, "Pool: You need to send some ETH");
     uint newDyad = lastEthPrice.mul(msg.value).div(100000000);
     require(newDyad >= minAmount, "Pool: mintDyad: minAmount not reached");
     dyad.mint(msg.sender, newDyad);
