d9aec1eafb06a534db00895e68fb86071f973b57
shafu.eth <selfouly@gmail.com>
2022-11-25 12:33:41 +0100

    mv file

--- test/Pool.Eike.t.sol
+++ test/Pool.Eike.t.sol
@@ -80,17 +80,18 @@ contract PoolTest is Test {
   }
 
   function setNfts() internal {
-    // overwrite id, xp, deposit, balance for each nft
-    overwriteNft(0, 2161, 146,  3920);
-    overwriteNft(1, 7588, 4616, 7496);
+    // overwrite id, xp, deposit, balance for each nft to the hard-coded
+    // values in the google sheet
+    overwriteNft(0, 2161, 146,  3920 );
+    overwriteNft(1, 7588, 4616, 7496 );
     overwriteNft(2, 3892, 2731, 10644);
-    overwriteNft(3, 3350, 4515, 2929);
-    overwriteNft(4, 3012, 2086, 3149);
-    overwriteNft(5, 5496, 7241, 7127);
-    overwriteNft(6, 8000, 8197, 7548);
-    overwriteNft(7, 7000, 5873, 9359);
-    overwriteNft(8, 3435, 1753, 4427);
-    overwriteNft(9, 1079, 2002, 244);
+    overwriteNft(3, 3350, 4515, 2929 );
+    overwriteNft(4, 3012, 2086, 3149 );
+    overwriteNft(5, 5496, 7241, 7127 );
+    overwriteNft(6, 8000, 8197, 7548 );
+    overwriteNft(7, 7000, 5873, 9359 );
+    overwriteNft(8, 3435, 1753, 4427 );
+    overwriteNft(9, 1079, 2002, 244  );
   }
 
   function testSyncEikesEquationsBurn() public {
