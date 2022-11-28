dc9905c0f5ec9cb238cd5f3c15785a96ceeeb793
shafu.eth <selfouly@gmail.com>
2022-11-28 19:00:19 +0100

    do randmo stuff

--- test/Launch.t.sol
+++ test/Launch.t.sol
@@ -33,6 +33,9 @@ contract LaunchTest is Test {
   address public addr1;
   address public addr2;
 
+  // needed, so we can receive eth transfers
+  receive() external payable {}
+
   function setUp() public {
     dyad       = new DYAD();
     dNFT _dnft = new dNFT(address(dyad), true); // with insider allocation
@@ -89,5 +92,61 @@ contract LaunchTest is Test {
     dnft.withdraw(NUMBER_OF_INSIDER_NFTS+1, 3 ether);
 
     pool.sync();
+  }
+
+  // very self explanatory I think. Do random stuff and see if it breaks.
+  // I think you call that fuzzy testing, lol :D
+  function testDoRandomStuffAndSync() public {
+    pool.sync();
+
+    // mint nfts
+    uint id1 = dnft.mintNft{value: 5 ether}(address(this));
+    vm.prank(addr1);
+    uint id2 = dnft.mintNft{value: 12 ether}(addr1);
+    vm.prank(addr1);
+    uint id3 = dnft.mintNft{value: 5 ether}(addr1);
+    vm.prank(addr2);
+    uint id4 = dnft.mintNft{value: 14 ether}(addr2);
+    vm.prank(addr2);
+    uint id5 = dnft.mintNft{value: 5 ether}(addr2);
+    uint id6 = dnft.mintNft{value: 8 ether}(address(this));
+    uint id7 = dnft.mintNft{value: 5 ether}(address(this));
+
+    pool.sync();
+
+    // do some withdraws
+    dnft.withdraw(id1, 2 ether);
+    dnft.withdraw(id1, 1 ether);
+    vm.prank(addr1);
+    dnft.withdraw(id2, 66626626262662);
+    vm.prank(addr1);
+    dnft.withdraw(id3, 4 ether);
+    vm.prank(addr2);
+    dnft.withdraw(id4, 100000000000);
+    vm.prank(addr2);
+    dnft.withdraw(id5, 5 ether);
+    dnft.withdraw(id6, 2 ether);
+    dnft.withdraw(id7, 4444444444444);
+
+    pool.sync();
+
+    // do some deposits
+    dyad.approve(address(dnft), 5 ether);
+    dnft.deposit(id1, 1 ether);
+    dnft.deposit(id1, 5000000);
+    vm.prank(addr2);
+    dyad.approve(address(dnft), 5 ether);
+    vm.prank(addr2);
+    dnft.deposit(id4, 1000);
+
+    for(uint i = 0; i < 4; i++) { pool.sync(); }
+
+    // do some redeems
+    dyad.approve(address(pool), 5 ether);
+    pool.redeem(100000002);
+    pool.redeem(100000202);
+    pool.redeem(3000000202);
+
+    for(uint i = 0; i < 4; i++) { pool.sync(); }
   }
 }
