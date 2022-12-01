8f11cb4b02d4782e106464bfee31920affca8f17
shafu.eth <selfouly@gmail.com>
2022-12-01 10:14:49 +0100

    refactor deployment scripts

--- script/Deploy.Goerli.s.sol
+++ script/Deploy.Goerli.s.sol
@@ -2,33 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import {IdNFT} from "../src/interfaces/IdNFT.sol";
-import {dNFT} from "../src/dNFT.sol";
-import "../src/dyad.sol";
-import "../src/pool.sol";
-
-address constant PRICE_ORACLE_GOERLI = 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e;
-// $1 deposit minimum
-uint constant DEPOSIT_MINIMUM = 1000000000000000000;
+import {Deployment} from "./DeploymentLibrary.sol";
 
 // Pseudo-code, may not compile.
 contract DeployGoerli is Script {
-  uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
   function run() public {
-    vm.startBroadcast(deployerPrivateKey);
-
-    DYAD dyad = new DYAD();
-
-    dNFT _dnft = new dNFT(address(dyad), DEPOSIT_MINIMUM, true); // with insider alloc
-    IdNFT dnft = IdNFT(address(_dnft));
-
-    Pool pool = new Pool(address(dnft), address(dyad), PRICE_ORACLE_GOERLI);
-
-    dnft.setPool(address(pool));
-    dyad.setMinter(address(pool));
-
-    vm.stopBroadcast();
+      address ORACLE_GOERLI   = 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e;
+      uint    DEPOSIT_MINIMUM = 1000000000000000000; // $l deposit minimum
+      new Deployment().deploy(ORACLE_GOERLI, DEPOSIT_MINIMUM, true);
   }
 }
 
