760cb8634077f35f5519d2fc015af5a512dd322b
shafu.eth <selfouly@gmail.com>
2022-12-22 00:29:18 +0100

    rm console

--- src/core/Pool.sol
+++ src/core/Pool.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "forge-std/console.sol";
 import {DYAD} from "./dyad.sol";
 import {IAggregatorV3} from "../interfaces/AggregatorV3Interface.sol";
 import {IdNFT} from "../interfaces/IdNFT.sol";
