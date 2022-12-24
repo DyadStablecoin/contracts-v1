fa889e5751036f8932a4f30818686686ba4c8c15
0xClandestine <clandestine.eth@proton.me>
2022-12-24 15:44:22 -0600

    ♻️added OZ ERC20Permit to DYAD

--- src/core/Dyad.sol
+++ src/core/Dyad.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract DYAD is ERC20, ERC20Burnable, Ownable {
-  constructor() ERC20("DYAD Stablecoin", "DYAD") {}
+contract DYAD is Ownable, ERC20, ERC20Burnable, ERC20Permit  {
+  constructor() 
+    ERC20("DYAD Stablecoin", "DYAD") 
+    ERC20Permit("DYAD Stablecoin") 
+  {}
 
   function mint(address to, uint256 amount) public onlyOwner {
       _mint(to, amount);
