dad0e5406984a918966a5f8a5f2122d1566bcf79
shafu <selfouly@gmail.com>
2023-01-06 19:26:03 +0100

    refactor

--- src/core/Dyad.sol
+++ src/core/Dyad.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DYAD is ERC20, Ownable {
   constructor() ERC20("DYAD Stablecoin", "DYAD") {}
