e4861887cb811db89ac048db231c007fcaa59393
shafu <selfouly@gmail.com>
2023-01-06 23:28:21 +0100

    rm import

--- src/core/dNFT.sol
+++ src/core/dNFT.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
