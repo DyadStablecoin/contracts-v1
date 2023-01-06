a79104be1c246fd187605cce46caac180a43f5f2
shafu <selfouly@gmail.com>
2023-01-06 21:15:58 +0100

    private

--- src/core/dNFT.sol
+++ src/core/dNFT.sol
@@ -74,9 +74,9 @@ contract dNFT is ERC721Enumerable, ERC721Burnable, ReentrancyGuard {
   mapping(uint => Nft) public idToNft;
 
   // Map dNFT id to the last block that deposit was called on.
-  // Needed to avoid deposit+withdraw in the same block, which enables
+  // Needed to avoid deposit + withdraw in the same block, which enables
   // different flash loan attacks.
-  mapping(uint => uint) public idToBlockOfLastDeposit;
+  mapping(uint => uint) private _idToBlockOfLastDeposit;
 
   DYAD public dyad;
   IAggregatorV3 internal oracle;
@@ -232,7 +232,7 @@ contract dNFT is ERC721Enumerable, ERC721Burnable, ReentrancyGuard {
       uint id,
       uint amount
   ) external onlyNFTOwner(id) amountNotZero(amount) returns (uint) {
-      if (idToBlockOfLastDeposit[id] == block.number) { revert CannotDepositAndWithdrawInSameBlock(); }
+      if (_idToBlockOfLastDeposit[id] == block.number) { revert CannotDepositAndWithdrawInSameBlock(); }
       Nft storage nft = idToNft[id];
       if (amount.toInt256() > nft.deposit) { revert ExceedsDepositLimit(amount); }
       uint updatedBalance  = dyad.balanceOf(address(this)) - amount;
@@ -255,7 +255,7 @@ contract dNFT is ERC721Enumerable, ERC721Burnable, ReentrancyGuard {
       uint id, 
       uint amount
   ) external amountNotZero(amount) returns (uint) {
-      idToBlockOfLastDeposit[id] = block.number;
+      _idToBlockOfLastDeposit[id] = block.number;
       Nft storage nft = idToNft[id];
       if (amount > nft.withdrawn) { revert ExceedsWithdrawalLimit(amount); }
       nft.deposit   += amount.toInt256();
